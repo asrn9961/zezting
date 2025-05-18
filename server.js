b359cef25266ef116de1fadc1d9a88cff1808346
asrn9961 <84643603+asrn9961@users.noreply.github.com>
2025-05-18 11:57:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
+app.use(express.static(path.join(__dirname)));
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
@@ -15,15 +18,17 @@ const io = new Server(server, {
 const onlineUsers = {};
 
 io.on("connection", (socket) => {
-  // Temporarily assign a placeholder name
+  // Temporary name until IP is registered
   onlineUsers[socket.id] = "Loading...";
 
+  // Receive IP from client and use as name
   socket.on("register-ip", (ip) => {
     onlineUsers[socket.id] = ip;
     io.emit("user-list", Object.values(onlineUsers));
     socket.emit("your-name", ip);
   });
 
+  // Handle sending message
   socket.on("send-message", ({ to, message }) => {
     for (const id in onlineUsers) {
       if (onlineUsers[id] === to) {
@@ -36,12 +41,19 @@ io.on("connection", (socket) => {
     }
   });
 
+  // On disconnect
   socket.on("disconnect", () => {
     delete onlineUsers[socket.id];
     io.emit("user-list", Object.values(onlineUsers));
   });
 });
 
+// Serve the HTML page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Start server
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
